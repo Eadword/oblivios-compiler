59ba3d54ab17333fa1f7c4ecb1e066e2ee114ca9
Eadword <matt@xram.co>
2016-04-28 22:21:19 -0700

    Able to parse macros from test input and report malformed inputs

--- preprocessor.cpp
+++ preprocessor.cpp
@@ -8,14 +8,32 @@ void Preprocessor::run(std::string& data) {
 }
 
 std::map<std::string, std::string> Preprocessor::getMacros(std::string data) {
-    std::map<std::string, std::string> macros;
-    std::regex macro("#(.*)\n");
+    const std::regex macro_line_regex("#(.*)\\n");
+    const std::regex macro_regex("([A-Z_a-z]\\w*)[ \\t\\v]+(.*)");
+    const std::sregex_iterator iterator_end = std::sregex_iterator();
 
-    std::smatch result;
-    while(std::regex_search(data, result, macro)) {
-        std::cout << "[" << result[1] << "]\n";
-        data = result.suffix().str();
+    std::map<std::string, std::string> macros;
+
+    for(std::sregex_iterator i = std::sregex_iterator(data.begin(), data.end(), macro_line_regex);
+        i != iterator_end; ++i)
+    {
+        std::string line = (*i)[1];
+        std::smatch macro;
+
+        if(std::regex_match(line, macro, macro_regex)) {
+            if(macros.find(macro[1]) == macros.end())
+                macros[macro[1]] = macro[2];
+            else std::cout << "Redefinition of macro identifier \"" << macro[1] << '\"' << std::endl;
+        }
+        else {
+            std::cout << "Could not recognize macro definition \"" << line << "\"" << std::endl;
+        }
     }
+
+#ifdef DEBUG
+    for(auto i = macros.begin(); i != macros.end(); ++i)
+        std::cout << "Found macro: #" << i->first << ' ' << i->second << std::endl;
+#endif
 
     return macros;
 }
