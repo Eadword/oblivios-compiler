536ea34b299c7b62bd7d93981f1032e24b05e7eb
Eadword <matt@xram.co>
2016-07-14 15:33:37 -0700

    Updated locations to new standard

--- include/location.h
+++ include/location.h
@@ -1,15 +1,22 @@
 #pragma once
 
+//TODO: use namespace?
 enum class Location : uint8_t {
-#define X(val) val
-#include "locations"
-#undef X
+    #define X(name, str, dstval, srcval) name
+    #include "locations"
+    #undef X
 };
 
 const std::vector<std::string> Location_Strings {
-#define X(val) #val
-#include "locations"
-#undef X
+    #define X(name, str, dstval, srcval) str
+    #include "locations"
+    #undef X
+};
+
+const std::map<Location, std::pair<int8_t, int8_t>> Location_Vals {
+    #define X(name, str, dstval, srcval) {Location::name, {dstval, srcval}}
+    #include "locations"
+    #undef X
 };
 
 inline std::string LocationToString(Location l) {
@@ -17,6 +24,7 @@ inline std::string LocationToString(Location l) {
 }
 
 inline Location LocationFromString(const std::string& l) {
+    if(l.empty()) return Location::NONE;
     auto loc = std::find(Location_Strings.begin(), Location_Strings.end(), l);
     if(loc == Location_Strings.end()) throw std::invalid_argument("Location " + l + " is not valid");
 
